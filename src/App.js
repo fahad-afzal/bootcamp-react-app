9f139b8994954f3d3657fbab2f4fe4cca9034d64
Fahad Afzal <fahadafzal00@gmail.com>
2020-09-15 20:46:30 +0500

    update App.Js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import Dinner from './dinner.js';
 
 function App() {
   return (
-    <div>
-       Hello World
-    </div>
+    <div classname="App">
+      <Dinner dishName="Chicken Masala" sweetdish="Kheer"/>
+      <hr/>
+      <Dinner dishName="Chicken Karahi" sweetdish="Gaajar ka halwa"/>
+      <hr/>
+      <Dinner dishName="Chicken Chaamp" sweetdish="Jalebi"/>    
+      </div>
   );
 }
 
